137a8ba0c923ff72cdc17670eeb6c989576d29f1
Suthan kathir <suthancode2gmail.com>
2022-01-16 22:33:11 -0500

    Main choice created

--- server.js
+++ server.js
@@ -10,5 +10,78 @@ function init() {
     console.log("***********************************************")
     console.log(" ❤ ❤ ❤   Welcome to Employee Manager! ❤  ❤  ❤")
     console.log("***********************************************")
+    mainMenu()
 
+}
+
+function mainMenu(){
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "What would you like to do?",
+            choices: [ "View Departments",  "View Roles", "View Employees",
+                        "Add Department", "Add Role" ,"Add Employee",
+                        "Update employee role", "Update employee managers",
+                        "View by manager", "View by department",
+                        "Delete Department", " Delete Roles", "Delete employee",
+                        "total budget" , "budget by department", " Exit Employee Tracker"],
+                        pageSize: 10
+        }
+    ])
+    .then(response =>{
+        console.log(response)
+        let userChoice = response.choice;
+        console.log(userChoice)
+        switch( userChoice ) {
+            case "View Departments":
+                viewDept();
+                 break;
+             case "View Roles":
+                viewRoles();
+                 break;
+            case "View Employees":
+                viewEmployees();
+                break;
+             case "Add Department":
+                addDDept();
+                break;
+            case "Add Role":
+                addRole();
+                break;
+            case "Add Employee":
+                addEmployee();
+                 break;
+            case  "Update employee role":
+                updateEmpRole();
+                 break;
+            case "Update employee managers":
+                updatEmpman();
+                break;
+            case "View by manager":
+                viewByman();
+                break;
+            case "View by department":
+                viewbyDept();
+                break;
+            case  "Delete Department":
+                delDept();
+                break;
+            case "Delete Roles":
+                delRole();
+                break;
+             case "Delete employee":
+                delEmp();
+                break;
+            case "total budget":
+                tolBudget();
+                 break;
+            case   "budget by department":
+                 budbyDept();
+                 break;
+            case" Exit Employee Tracker":
+                quit();
+                break;
+    }
+})
 }
