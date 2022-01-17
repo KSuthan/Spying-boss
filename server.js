d3b8f9de791b77f026aa95713e76fb4e7d743ada
Suthan kathir <suthancode2gmail.com>
2022-01-16 22:31:19 -0500

    Server.js , Connection.js created

---
+++ server.js
@@ -0,0 +1,14 @@
+
+const inquirer = require("inquirer");
+const { connection } = require("./db");
+const db = require("./db")
+
+
+init();
+
+function init() {
+    console.log("***********************************************")
+    console.log(" ❤ ❤ ❤   Welcome to Employee Manager! ❤  ❤  ❤")
+    console.log("***********************************************")
+
+}
